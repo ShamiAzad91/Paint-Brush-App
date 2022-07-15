6b559e8a652498fbb2f6a138d695bccda9e11c91
ShamiAzad91 <103378609+ShamiAzad91@users.noreply.github.com>
2022-07-15 23:36:01 +0530

    Add files via upload

---
+++ db/controllers.js
@@ -0,0 +1,115 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true });
+
+const artistSchema = mongoose.Schema({
+  artistName: String,
+  paintings: Array
+})
+
+const PaintingModel = mongoose.model('PaintingModel', artistSchema);
+
+const getPaintings = (name, callback) => {
+  PaintingModel.findOne({artistName: name}, (err, doc) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, doc);
+    }
+  })
+}
+
+const savePainting = (name, paintingName, paintingData, callback) => {
+  PaintingModel.findOne({artistName: name}, (err, doc) => {
+    if (err) {
+      callback(err);
+    } else {
+      if (doc) {
+        let paintings = doc.paintings;
+        let exists = false;
+        for (let i = 0; i < paintings.length; i++) {
+          if (paintings[i][0] === paintingName) {
+            paintings[i][1] = paintingData;
+            exists = true;
+          }
+        }
+        if (!exists) {
+          paintings.push([paintingName, paintingData]);
+        }
+        PaintingModel.updateOne({artistName: name}, {paintings: paintings}, (err, doc) => {
+          if (err) {
+            callback(err);
+          } else {
+            getPaintings(name, (err, docs) => {
+              if (err) {
+                callback(err);
+              } else {
+                callback(null, docs);
+              }
+            })
+
+          }
+        })
+      } else {
+       let artist = new PaintingModel({artistName: name, paintings: [[paintingName, paintingData]]})
+      //  console.log(paintingName, paintingData)
+      //   console.log(artist);
+        artist.save((err) => {
+          if (err) {
+            callback(err);
+          } else {
+            getPaintings(name, (err, docs) => {
+              if (err) {
+                callback(err);
+              } else {
+                callback(null, docs)
+              }
+            })
+          }
+        })
+      }
+    }
+  })
+};
+
+
+
+const deletePainting = (name, paintingName, callback) => {
+  PaintingModel.findOne({artistName: name}, (err, doc) => {
+    if (err) {
+      callback(err);
+    } else {
+
+      if (doc) {
+        let paintings = doc.paintings;
+        for (let i = 0; i < paintings.length; i++) {
+          if (paintings[i][0] === paintingName) {
+            paintings.splice(i, 1);
+          }
+        }
+        PaintingModel.updateOne({artistName: name}, {paintings: paintings}, (err, doc) => {
+          if (err) {
+            callback(err);
+          } else {
+            callback(null, doc);
+          }
+        })
+      }
+    }
+  })
+}
+
+module.exports = {
+  savePainting,
+  getPaintings,
+  deletePainting
+}
---
+++ index.js
@@ -0,0 +1,49 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const controllers = require('./db/controllers.js');
+
+const app = express();
+app.use(bodyParser.json({limit: '99mb'}));
+app.use(bodyParser.urlencoded({limit: '99mb', extended: true}));
+app.use(express.static(path.join(__dirname, './client/dist')));
+
+app.get('/api/:artist', (req, res) => {
+  controllers.getPaintings(req.params.artist, (err, doc) => {
+    if (err) {
+      res.sendStatus(400);
+    } else {
+      res.status(200).send(doc);
+    }
+  })
+})
+
+app.post('/api/:artist', (req, res) => {
+  controllers.savePainting(req.params.artist, req.body.paintingName, req.body.paintingData, (err, doc) => {
+    if (err) {
+      res.sendStatus(400);
+    } else {
+      res.status(201).send(doc);
+    }
+  })
+})
+
+app.delete('/api/:artist/:paintingName', (req, res) => {
+  controllers.deletePainting(req.params.artist, req.params.paintingName, (err) => {
+    if (err) {
+      res.sendStatus(400);
+    } else {
+      res.sendStatus(202);
+    }
+  })
+})
+
+
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, () => {
+  console.log(`Web server running on: http://localhost:${PORT}`);
+});
